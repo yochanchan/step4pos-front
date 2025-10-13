8adf73e19835628e8a8cb91548d1c683470a4b29
yochanchan <arai0201@gmail.com>
2025-10-13 20:57:58 +0900

    first commit

---
+++ src/app/name/page.tsx
@@ -0,0 +1,14 @@
+import './style.css'
+
+export default function Name() {
+  return (
+    <main>
+      <h1 className="title">Name root</h1>
+      <p className="msg">/name/○○で次のページにアクセスする</p>
+      <div>
+        <a href="/">back</a>
+      </div>
+    </main>
+
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,137 @@
-import Image from "next/image";
+'use client'
+
+import { useRef, useState } from 'react';
+import { preconnect } from 'react-dom';
+
+type DealPayload = {
+  cartpayload: number[];
+  amountpayload: number;
+}
+
+
+
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  type Nakami = {
+    code: number;
+    name: string;
+    price: number;
+  }
+
+
+
+  const [name, setName] = useState<string>("-");
+  const [price, setPrice] = useState<number>(0);
+  const [nakami, addNakami] = useState<Nakami[]>([]);
+  const itemCode = useRef<HTMLInputElement>(null!);
+  const cartRef = useRef<number[]>([])
+
+  const APIURL = process.env.NEXT_PUBLIC_API_ENDPOINT
+
+  let pre_info = { name: "-", kazu: 0, price: 0, ammount: 0 };
+
+  /* 読み込み部分 */
+  function setCode() {
+    fetchCode().then(res => {
+      setName(res.name)
+      setPrice(res.price)
+    });
+  }
+
+  async function fetchCode() {
+    const param = "/item?prd_code=";
+    const url = APIURL + param
+    const res = await fetch(
+      url + itemCode.current.value,
+      { cache: 'no-store' }
+    );
+    const result = await res.json();
+    pre_info = result;
+    return result;
+  }
+
+
+  /* 追加部分 */
+  function toCart() {
+    cartRef.current.push(Number(itemCode.current.value))
+    console.log(cartRef)
+  }
+
+
+  function toList() {
+    addNakami([
+      ...nakami,
+      { code: 1, name: name, price: price }
+    ]);
+    console.log(nakami)
+  }
+
+  function Cart() {
+
+    return (
+      <div>
+        <ul>
+          {nakami.map((i, k) => {
+            return (<li className="text-left" key={k}>{i.name}　x1　{i.price}円　{i.price}円</li>)
+          }
+          )}
+          <li></li>
+        </ul>
+      </div>
+    );
+  }
+
+
+  async function Deal() {
+    const param = "/deal";
+    const url = APIURL + param;
+    let amount = 0;
+    console.log(nakami.length)
+
+    for (let i = 0; i < nakami.length; i++) {
+      amount = amount + nakami[i].price;
+    }
+
+    const payload: DealPayload = {
+      cartpayload: cartRef.current,
+      amountpayload: amount
+    };
+    const res = await fetch(
+      url,
+      {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(payload)
+      }
+    );
+  }
+
+
+  return (
+    <main>
+      <div className="w-5/8 bg-gray-300">
+        <div className="grid grid-cols-2 gap-2">
+          <div className="m-2 p-4 border-solid bg-blue-100">
+            <form>
+              <input className="input"
+                type="number"
+                ref={itemCode}
+              />
+            </form>
+            <button className="btn my-3" onClick={setCode}>商品コード 読み込み</button>
+            <div className="my-3">{name}</div>
+            <div>{price}</div>
+            <button className="btn my-3" onClick={() => { toCart(); toList(); }}>追加</button>
+          </div>
+          <div className="m-2 p-4 bg-amber-100">
+            <p className="msg text-center">購入リスト</p>
+            <Cart />
+            <button className="btn" onClick={() => { Deal() }}>購入</button>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </div>
+    </main >
   );
+
 }
