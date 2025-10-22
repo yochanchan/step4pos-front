cf191f0b82356dee2f5ad2ae0d90f0687bd06d02
yochanchan <arai0201@gmail.com>
2025-10-23 08:03:08 +0900

    next.config修正

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,6 @@
 import type { NextConfig } from "next";
-
 const nextConfig: NextConfig = {
-  output: 'standalone',
-  env: {
-    // Reference a variable that was defined in the .env file and make it available at Build Time
-    API_ENDPOINT: process.env.API_ENDPOINT,
-  },
+  output: 'standalone'
 }
 
 export default nextConfig;
